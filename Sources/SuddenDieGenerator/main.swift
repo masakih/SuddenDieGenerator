98b0ba2fe81275070e809486950d8243df108271
masakih <masakih@users.sourceforge.jp>
2018-04-15 12:32:07 +0900

    コメントを追加

--- Sources/SuddenDieGenerator/main.swift
+++ Sources/SuddenDieGenerator/main.swift
@@ -7,14 +7,23 @@
 import Utility
 import SuddenDieGeneratorCore
 
+/// ベーストなるパーサ
+/// usage, overviewはhelpに使用される
 let parser = ArgumentParser(usage: "text", overview: "”Sudden die“ generator")
-let args = parser.add(positional: "text", kind: String.self, usage: "Base text")
+
+/// パーサに引数の指定を追加
+/// positionalは識別子であり、heplに使用される
+/// kindは引数の型
+/// usageはhelpに使用される
+let arguments = parser.add(positional: "text", kind: String.self, usage: "Base text")
 
 do {
     
+    /// コマンドライン引数からツール名を取り除きパーサにパースさせる
     let result = try parser.parse(Array(CommandLine.arguments.dropFirst()))
     
-    if let value = result.get(args) {
+    /// パースの結果からarguments(="text")の値を取り出す
+    if let value = result.get(arguments) {
         
         let generator = Generator(value)
         print(generator.generate())
