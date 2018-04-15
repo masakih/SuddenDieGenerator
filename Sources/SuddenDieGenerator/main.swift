ad7f22dd5d4096506e694fbf66bbc7141771beb9
masakih <masakih@users.sourceforge.jp>
2018-04-15 14:23:16 +0900

    ArgumentBinderの利用

--- Sources/SuddenDieGenerator/main.swift
+++ Sources/SuddenDieGenerator/main.swift
@@ -7,7 +7,19 @@
 import Utility
 import SuddenDieGeneratorCore
 
-/// ベーストなるパーサ
+/// 引数やオプションを保持するstruct
+struct SuddenDieOptions {
+    
+    var suffix: String?
+    
+    var text = ""
+}
+
+/// 引数やオプションを設定するのに利用するArgumentBinder
+/// bind時にSuddenDieOptionsが与えられる
+let binder = ArgumentBinder<SuddenDieOptions>()
+
+/// ベースとなるパーサ
 /// usage, overviewはhelpに使用される
 let parser = ArgumentParser(usage: "text", overview: "”Sudden die“ generator")
 
@@ -16,37 +28,42 @@ let parser = ArgumentParser(usage: "text", overview: "”Sudden die“ generator
 /// shortNameは "-" から始めなければならない
 /// kindはオプションの値の型
 /// usageはhelpに使用される
-let suffix = parser.add(option: "--suffix",
-                        shortName: "-s",
-                        kind: String.self,
-                        usage: "Suffix of text")
+///
+/// ArgumentBinderを利用し値を取得する
+binder.bind(option: parser.add(option: "--suffix",
+                               shortName: "-s",
+                               kind: String.self,
+                               usage: "Suffix of text"),
+            to: { options, suffix in
+                
+                options.suffix = suffix
+})
 
 /// パーサに引数の指定を追加
 /// positionalは識別子であり、heplに使用される
 /// kindは引数の型
 /// usageはhelpに使用される
-let arguments = parser.add(positional: "text", kind: String.self, usage: "Base text")
-
+///
+/// ArgumentBinderを利用し値を取得する
+binder.bind(positional: parser.add(positional: "text", kind: String.self, usage: "Base text"),
+            to: { options, text in
+                
+                options.text = text
+})
 
 do {
     
     /// コマンドライン引数からツール名を取り除きパーサにパースさせる
     let result = try parser.parse(Array(CommandLine.arguments.dropFirst()))
     
-    /// オプション --suffixの値を取り出す
-    var suffixValue: String?
-    if let value = result.get(suffix) {
-        
-        suffixValue = value
-    }
-    /// パースの結果からarguments(="text")の値を取り出す
-    if let value = result.get(arguments) {
-        
-        let generator = Generator(value)
-        print(generator.generate(suffix: suffixValue))
-    }
+    /// bindに利用するSuddenDieOptionsを用意
+    var options = SuddenDieOptions()
     
-    
+    /// パースの結果を用いてbindすることで与えたoptionsに値が設定される
+    binder.fill(result, into: &options)
+
+    let generator = Generator(options.text)
+    print(generator.generate(suffix: options.suffix))
     
 } catch let error as ArgumentParserError {
     
