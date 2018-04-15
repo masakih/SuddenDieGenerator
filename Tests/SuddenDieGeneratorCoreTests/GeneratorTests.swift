0aa26191a5e5374c01fcc9a117760e1714b0e752
masakih <masakih@users.sourceforge.jp>
2018-04-15 12:44:58 +0900

    suffixのテストを追加

--- Tests/SuddenDieGeneratorCoreTests/GeneratorTests.swift
+++ Tests/SuddenDieGeneratorCoreTests/GeneratorTests.swift
@@ -18,4 +18,9 @@ class SuddenDieGeneratorTests: XCTestCase {
         
         XCTAssertEqual(generator.generate(), "＿人人人人＿\n＞　test　＜\n￣Y^Y^Y￣")
     }
+    
+    func testGenerateSuffix() {
+        
+        XCTAssertEqual(generator.generate(suffix: "にゃん"), "＿人人人人人人人＿\n＞　testにゃん　＜\n￣Y^Y^Y^Y^Y^Y￣")
+    }
 }
